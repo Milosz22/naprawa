7ad614b4645b16ef2fca8015eebe36c41c155d14
Milosz22 <72401562+Milosz22@users.noreply.github.com>
2022-12-18 00:15:24 +0100

    Update test_app.py

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -4,40 +4,57 @@ from Christofides.christofides import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_load_graph_from_file_1(self):
+
+    def test_save_christo_graph_to_file_1(self):
         # Given
-        filepath = "xD.txt"
-        expected_result = {"0": {"1": 1, "2": 2}, "1": {"0": 1, "2": 2}, "2": {"0": 2, "1": 2}}
+        filepath = "test_save_1.txt"
+        graphToTest = {'0': {'1': 1, '2': 20, '3': 2}, '1': {'0': 1, '2': 28, '3': 5},
+                       '2': {'0': 20, '1': 28, '3': 32}, '3': {'0': 2, '1': 5, '2': 32}}
+        expected_result = str(christofides(graph(graphToTest)))
+        expected_result = "".join(
+            c for c in expected_result if c.isdigit())
 
         # When
-        result = load_graph_from_file(filepath)
+        save_christo_graph_to_file(graphToTest, filepath)
+        with open(filepath) as f:
+            result = f.read()
 
         # Then
         self.assertEqual(expected_result, result)
 
-    def test_load_graph_from_file_2(self):
+    def test_save_christo_graph_to_file_2(self):
         # Given
-        filepath = "xD.txt"
-        expected_result = {"0": {"1": 1, "2": 2, "3": 3}, "1": {"0": 1, "2": 2, "3": 3}, "2": {"0": 2, "1": 2, "3": 3},
-                           "3": {"0": 3, "1": 3, "2": 3}}
+        filepath = "test_save_2.txt"
+        graphToTest = {'0': {'1': 1, '2': 20, '3': 2}, '1': {'0': 1, '2': 28, '3': 5},
+                       '2': {'0': 20, '1': 28, '3': 32}, '3': {'0': 2, '1': 5, '2': 32}}
+        expected_result = str(christofides(graph(graphToTest)))
+        expected_result = "".join(
+            c for c in expected_result if c.isdigit())
 
         # When
-        result = load_graph_from_file(filepath)
+        save_christo_graph_to_file(graphToTest, filepath)
+        with open(filepath) as f:
+            result = f.read()
 
         # Then
         self.assertEqual(expected_result, result)
 
-    def test_load_graph_from_file_3(self):
+    def test_save_christo_graph_to_file_3(self):
         # Given
-        filepath = "xD.txt"
-        expected_result = {"0": {"1": 1}, "1": {"0": 1}}
+        filepath = "test_save_3.txt"
+        graphToTest = {'0': {'1': 2, '2': 270, '3': 1000}, '1': {'0': 2, '2': 15, '3': 120},
+                       '2': {'0': 270, '1': 15, '3': 50}, '3': {'0': 1000, '1': 120, '2': 50}}
+        expected_result = str(christofides(graph(graphToTest)))
+        expected_result = "".join(
+            c for c in expected_result if c.isdigit())
 
         # When
-        result = load_graph_from_file(filepath)
+        save_christo_graph_to_file(graphToTest, filepath)
+        with open(filepath) as f:
+            result = f.read()
 
         # Then
         self.assertEqual(expected_result, result)
-
 
 
 
